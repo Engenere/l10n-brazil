482f225301843f3a029140a81e0d6568c06f4b29
Antônio Neto <neto@engenere.one>
2025-07-22 21:14:49 -0300

    [ADD] l10n_br_check

---
+++ l10n_br_check/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Brazil - Checks Management",
+    "summary": "Brazilian Checks Management localization",
+    "version": "16.0.1.0.0",
+    "author": "Engenere, Odoo Community Association (OCA)",
+    "maintainers": ["antoniospneto"],
+    "website": "https://github.com/OCA/l10n-brazil",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_latam_check",
+        "l10n_br_base",
+    ],
+    "data": [
+        "views/account_payment_view.xml",
+    ],
+    "demo": [],
+}
---
+++ l10n_br_check/readme/DESCRIPTION.md
@@ -0,0 +1,6 @@
+# Brazilian Check UX Improvements
+
+This module extends **Check Management** to better match Brazilian banking practice.
+
+## Feature
+* **Bank selector** on cheque payments now displays the Brazilian Central Bank (BCB) code _before_ the name, e.g. `001 – Banco do Brasil`.
