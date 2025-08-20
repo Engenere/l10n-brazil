19ad5e6837a59982cbc3723621af48580ea928bb
Antônio Neto <neto@engenere.one>
2025-08-19 21:50:44 -0300

    [REF] l10n_br_sale: remove old fiscal tax onchange

--- l10n_br_sale/tests/test_l10n_br_sale.py
+++ l10n_br_sale/tests/test_l10n_br_sale.py
@@ -158,7 +158,6 @@ class L10nBrSaleBaseTest(TransactionCase):
             return
         sale_line._onchange_product_id_fiscal()
         sale_line._onchange_fiscal_taxes()
-        sale_line._onchange_fiscal_tax_ids()
 
     def _invoice_sale_order(self, sale_order):
         sale_order.action_confirm()
