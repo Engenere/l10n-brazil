0104dccbbf1ac39bf0a470114a136c70ab2e4fc1
Antônio Neto <neto@engenere.one>
2025-08-23 01:43:13 -0300

    [REF] l10n_br_account: clean tests

--- l10n_br_account/tests/test_document_date.py
+++ l10n_br_account/tests/test_document_date.py
@@ -11,87 +11,39 @@ from odoo.tests import TransactionCase
 from odoo.addons.l10n_br_fiscal.constants.fiscal import DOCUMENT_ISSUER_PARTNER
 
 
-class TestInvoiceDiscount(TransactionCase):
+class TestDocumentDate(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.company = cls.env.ref("l10n_br_base.empresa_lucro_presumido")
-
-        # set default user company
-        companies = cls.env["res.company"].search([])
-        cls.env.user.company_ids = [Command.set(companies.ids)]
-        cls.env.user.company_id = cls.company
-
-        cls.invoice_account_id = cls.env["account.account"].create(
-            {
-                "company_id": cls.company.id,
-                "account_type": "asset_receivable",
-                "code": "RECTEST",
-                "name": "Test receivable account",
-                "reconcile": True,
-            }
-        )
-
-        cls.invoice_journal = cls.env["account.journal"].create(
-            {
-                "company_id": cls.company.id,
-                "name": "Invoice Journal - (test)",
-                "code": "INVTEST",
-                "type": "sale",
-            }
-        )
-
-        cls.invoice_line_account_id = cls.env["account.account"].create(
-            {
-                "company_id": cls.company.id,
-                "account_type": "income",
-                "code": "705070",
-                "name": "Product revenue account (test)",
-            }
-        )
-
         cls.fiscal_operation_id = cls.env.ref("l10n_br_fiscal.fo_venda")
-        cls.fiscal_operation_id.deductible_taxes = True
 
         product_id = cls.env.ref("product.product_product_7")
 
         invoice_line_vals = [
-            (
-                0,
-                0,
+            Command.create(
                 {
                     "fiscal_operation_line_id": cls.env.ref(
                         "l10n_br_fiscal.fo_venda_revenda"
                     ).id,
-                    "account_id": cls.invoice_line_account_id.id,
                     "product_id": product_id.id,
                     "quantity": 1,
                     "price_unit": 1000.0,
-                    "discount_value": 100.0,
-                },
+                }
             )
         ]
 
-        cls.move_id = (
-            cls.env["account.move"]
-            .with_context(check_move_validity=False)
-            .create(
-                {
-                    "company_id": cls.company.id,
-                    "partner_id": cls.env.ref("base.res_partner_3").id,
-                    "document_type_id": cls.env.ref("l10n_br_fiscal.document_55").id,
-                    "document_serie_id": cls.env.ref(
-                        "l10n_br_fiscal.empresa_lc_document_55_serie_1"
-                    ).id,
-                    "journal_id": cls.invoice_journal.id,
-                    "invoice_user_id": cls.env.user.id,
-                    "fiscal_operation_id": cls.fiscal_operation_id.id,
-                    "move_type": "out_invoice",
-                    "currency_id": cls.company.currency_id.id,
-                    "invoice_line_ids": invoice_line_vals,
-                }
-            )
+        cls.move_id = cls.env["account.move"].create(
+            {
+                "partner_id": cls.env.ref("base.res_partner_3").id,
+                "document_type_id": cls.env.ref("l10n_br_fiscal.document_55").id,
+                "document_serie_id": cls.env.ref(
+                    "l10n_br_fiscal.empresa_lc_document_55_serie_1"
+                ).id,
+                "fiscal_operation_id": cls.fiscal_operation_id.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": invoice_line_vals,
+            }
         )
 
     def test_document_date(self):
@@ -106,7 +58,6 @@ class TestInvoiceDiscount(TransactionCase):
         original_date_without_time = original_date_in_user_tz.date()
 
         self.move_id.invoice_date = original_date.date()
-        self.move_id.fiscal_document_id._compute_document_date()
 
         self.assertEqual(
             self.move_id.fiscal_document_id.document_date.date(),
@@ -136,7 +87,6 @@ class TestInvoiceDiscount(TransactionCase):
         )
         original_date_without_time = original_date_in_user_tz.date()
         self.move_id.date = original_date.date()
-        self.move_id.fiscal_document_id._compute_date_in_out()
 
         self.assertEqual(
             self.move_id.fiscal_document_id.date_in_out.date(),
@@ -148,7 +98,6 @@ class TestInvoiceDiscount(TransactionCase):
         self.move_id.issuer = DOCUMENT_ISSUER_PARTNER
         new_date = datetime.now() - timedelta(days=2)
         self.move_id.fiscal_document_id.date_in_out = new_date
-        self.move_id.fiscal_document_id._inverse_date_in_out()
         self.assertEqual(
             self.move_id.date,
             new_date.date(),
