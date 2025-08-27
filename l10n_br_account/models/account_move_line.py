adcda670772beb9c8f6ae760566b5ffb2537fb04
Antônio Neto <neto@engenere.one>
2025-08-26 23:08:20 -0300

    WIP: preserve tax ids

--- l10n_br_account/models/account_move_line.py
+++ l10n_br_account/models/account_move_line.py
@@ -167,8 +167,23 @@ class AccountMoveLine(models.Model):
         original_indexes = [idx for idx, _ in sorted_indexed_vals_list]
         vals_list = [val for _, val in sorted_indexed_vals_list]
 
-        # Create the records
-        result = super().create(vals_list)
+        # Evitar recomputação, limitação do inhertis?
+        # TODO fazer para todos os campos?
+        with_tax, without_tax = [], []
+        for vals in vals_list:
+            if "fiscal_tax_ids" in vals:
+                with_tax.append(vals)
+            else:
+                without_tax.append(vals)
+
+        result = self.browse()
+        if without_tax:
+            result |= super().create(without_tax)
+
+        if with_tax:
+            result |= super(
+                AccountMoveLine, self.with_context(preserve_fiscal_tax_ids=True)
+            ).create(with_tax)
 
         # Initialize the inverted index list with the same length as the original list
         inverted_index = [0] * len(original_indexes)
