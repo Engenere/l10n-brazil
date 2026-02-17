0a8d4170dff8f80d02e34f1444ac64b98339d30f
Antônio Neto <neto@engenere.one>
2026-02-17 17:21:50 -0300

    [FIX] l10n_br_fiscal_dfe, l10n_br_fiscal: corrige mapeamento cSitNFe
    Os valores de cSitNFe no resNFe estavam invertidos entre
    Cancelada e Denegada. Conforme o XSD da SEFAZ (resNFe_v1.01.xsd):
    - 1 = Uso autorizado
    - 2 = Uso denegado
    - 3 = NF-e Cancelada

--- l10n_br_fiscal/constants/mdfe.py
+++ l10n_br_fiscal/constants/mdfe.py
@@ -2,10 +2,13 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 SIT_NFE_AUTORIZADA = ("1", "Autorizada")
-SIT_NFE_CANCELADA = ("2", "Cancelada")
-SIT_NFE_DENEGADA = ("3", "Denegada")
+SIT_NFE_DENEGADA = ("2", "Denegada")
+SIT_NFE_CANCELADA = ("3", "Cancelada")
 
-SITUACAO_NFE = [SIT_NFE_AUTORIZADA, SIT_NFE_CANCELADA, SIT_NFE_DENEGADA]
+# Situação da NF-e: 1=Uso autorizado; 2=Uso denegado;
+# 3=NF-e Cancelada;
+
+SITUACAO_NFE = [SIT_NFE_AUTORIZADA, SIT_NFE_DENEGADA, SIT_NFE_CANCELADA]
 
 OP_TYPE_ENTRADA = ("0", "Entrada")
 OP_TYPE_SAIDA = ("1", "Saída")
