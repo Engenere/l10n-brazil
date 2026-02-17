74a235d8cf861bad584a441e79821b65ed3f9a3d
Antônio Neto <neto@engenere.one>
2026-02-17 15:15:25 -0300

    [REM] l10n_br_nfe: remove unused mde.py constants file
    mde.py duplicated the same manifestation constants already defined in
    mdest.py and was not imported anywhere.

--- l10n_br_nfe/constants/mdest.py
+++ l10n_br_nfe/constants/mdest.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2023 KMEE Informática LTDA
+# Copyright 2026 Engenere (<https://engenere.one>).
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 MANIF_CIENTE = "ciente"
